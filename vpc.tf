f520a1a931f3be07b45b4a02365a93e98c795793
TAKANO Mitsuhiro <takano32@gmail.com>
2024-02-20 00:28:18 +0900

    VPC: Use `ap-northeast-1{a,c,d}` zones

--- vpc.tf
+++ vpc.tf
@@ -10,12 +10,13 @@ module "vpc" {
 
   name                 = "${var.eks-cluster-name}-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   database_subnets     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
+  enable_vpn_gateway   = false
   enable_dns_hostnames = true
 
   public_subnet_tags = {
